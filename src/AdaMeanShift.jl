3d8fb7ed91ee5091bbd0d6c019e6c67cbd6abc5b
Francesco Alemanno <francescoalemanno710@gmail.com>
2019-06-05 19:46:44 +0200

    better names, more docstrings

--- src/AdaMeanShift.jl
+++ src/AdaMeanShift.jl
@@ -1,7 +1,7 @@
 module AdaMeanShift
 
     using Statistics, LinearAlgebra, SpecialFunctions, StaticArrays
-    export genms, genintensity, meanshift!
+    export atomic_meanshift, atomic_intensity, meanshift!
     @inline function Kern(x::AbstractVector{fT},h::AbstractVector{fT}) where fT
         max(zero(fT),one(fT) - norm(@inbounds x ./ max.(h,one(fT)))^2)
     end
@@ -18,12 +18,28 @@ module AdaMeanShift
         #round(Int,p[i]-h[i]-1):round(Int,p[i]+h[i]+1)
         @inbounds clampdim(M,round(Int,p[i]-h[i]-1),i):clampdim(M,round(Int,p[i]+h[i]+1),i)
     end
-    function genms(M,p,h,isotropy,smoothing)
+
+
+
+"""
+        atomic_meanshift(M,p,h,isotropy,smoothing) -> NamedTuple(pnew,hnew,modeval,delta)
+
+Performs a single iteration of meanshift over a single particle
+
+# Arguments
+- `M` density tensor
+- `p` initial position for test particle
+- `h` initial particle standard deviation
+- `isotropy` scalar 0 -> 1, 0 is completely anisotropic, completely isotropic
+- `smoothing` scalar 0 -> 1, regularization term for noisy density tensors
+
+"""
+    function atomic_meanshift(M,p,h,isotropy,smoothing)
         N=length(p)
         @assert N==length(h)
-        genms(M,SVector{length(p)}(p),SVector{length(h)}(h),isotropy,smoothing)
+        atomic_meanshift(M,SVector{length(p)}(p),SVector{length(h)}(h),isotropy,smoothing)
     end
-    @generated function genms(M::Mty,p::K,h::K,isotropy,smoothing) where {T,N,K<:StaticArray,Mty<:AbstractArray{T,N}}
+    @generated function atomic_meanshift(M::Mty,p::K,h::K,isotropy,smoothing) where {T,N,K<:StaticArray,Mty<:AbstractArray{T,N}}
         D=(8//N^2 + 6//N +1)
         scale= sqrt(N * ((2//N + 1)*(4//N + 1)*(6//N + 1))//D)
         invvolume=gamma(N/2+1)*pi^(-N/2)
@@ -62,13 +78,26 @@ module AdaMeanShift
             end
         end
     end
-    function genintensity(M,p,h)
+
+    """
+            atomic_intensity(M::Mty,p::K,h::K) where {T,N,K<:StaticArray,Mty<:AbstractArray{T,N}}
+
+    Calculates the integrated intensity on the density tensor `M` over a ellipsoidal region caracterized by `p` region center, `h` semiaxis
+
+    # Arguments
+    - `M` density tensor
+    - `p` region center
+    - `h` semiaxis lengths
+
+    Note that both `p`,`h` should be of type Vector or StaticVector with the same length.
+    """
+    function atomic_intensity(M,p,h)
         N=length(p)
         @assert N==length(h)
-        genintensity(M,SVector{length(p)}(p),SVector{length(h)}(h))
+        atomic_intensity(M,SVector{length(p)}(p),SVector{length(h)}(h))
     end
 
-    @generated function genintensity(M::Mty,p::K,h::K) where {T,N,K<:StaticArray,Mty<:AbstractArray{T,N}}
+    @generated function atomic_intensity(M::Mty,p::K,h::K) where {T,N,K<:StaticArray,Mty<:AbstractArray{T,N}}
         Z=zero(T)
         O=one(T)
         quote
@@ -86,30 +115,25 @@ module AdaMeanShift
         end
     end
 
-    """
+
+"""
     meanshift!(M, P, h, w, hmax; isotropy=½, maxit=∞, rtol=√ϵ, smoothing=1)
 
     Performs MeanShift on a swarm of particles over a given density matrix locating all modes and their scale.
 
-    Arguments
-    ================================
+# Arguments
+- `M` is the density tensor over which particles evolve.
+- `P` is a julia vector of StaticVectors describing all positions.
+- `h` is a julia vector of StaticVectors describing all standard deviations.
+- `w` is a julia vector which will contain the new modes intensity estimates.
 
-        `M` is the density tensor over which particles evolve.
-        `P` is a julia vector of StaticVectors describing all positions.
-        `h` is a julia vector of StaticVectors describing all standard deviations.
-        `w` is a julia vector which will contain the new modes intensity estimates.
-        `hmax` is the maximum norm that standard deviations "h" can have.
+# Keyword Arguments
+- `isotropy` is a scalar [0,1] set to 1 for isotropic kernel, < 1 anisotropy along coordinate axis is allowed.
+- `maxit` is a integer for the maximum number of meanshift iterations.
+- `rtol` is the absolute tolerance to declare a particle as converged.
+- `smoothing` is a regularization term for density tensors with noise.
 
-    Keyword Arguments
-    ================================
-
-        `isotropy` is a scalar [0,1] set to 1 for isotropic kernel,
-                           < 1 anisotropy along coordinate axis is allowed.
-        `maxit` is a integer for the maximum number of meanshift iterations.
-        `rtol` is the absolute tolerance to declare a particle as converged.
-        `smoothing` is a regularization term for density tensors with noise.
-
-    """
+"""
     function meanshift!(M::Mty,P::AbstractVector{K},h::AbstractVector{K},w::AbstractVector{T},
         hmax::T; isotropy::T = T(1/2),maxit::T = T(Inf),rtol::T=sqrt(eps(T)),smoothing::T=one(T)) where {T,N,K<:StaticArray,Mty<:AbstractArray{T,N}}
         a=@elapsed Threads.@threads for i in eachindex(P)
@@ -119,7 +143,7 @@ module AdaMeanShift
                 if norm(h[i])>hmax
                     h[i]=h[i] ./ norm(h[i]) .* hmax
                 end
-                @inbounds P[i],h[i],w[i],delta=genms(M,P[i],h[i],isotropy)
+                @inbounds P[i],h[i],w[i],delta=atomic_meanshift(M,P[i],h[i],isotropy)
                 cnt+=1
             end
         end
