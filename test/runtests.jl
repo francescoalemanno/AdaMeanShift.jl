37937fc885f58016694a07a2b40f00ea39dbcf77
Francesco Alemanno <francescoalemanno710@gmail.com>
2019-06-06 07:32:39 +0200

    micro test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
 using AdaMeanShift
+using StaticArrays
 using Test
 
 @testset "AdaMeanShift.jl" begin
-    # Write your own tests here.
+    vx=-20:0.1:20
+    vy=0:0.1:30
+    M=[exp(-(x-7)^2/(2*3)-(y-12)^2/(2*2)) for x in vx, y in vy]
+    posmax=Tuple(findmax(M)[2])
+    posmax.*1.0
+    P,h,w,δ = atomic_meanshift(M, @SVector([100.0,100.0]) , @SVector([50.0,50.0]), 0.0,0.0) 
+    for i in 1:500
+        P,h,w,δ = atomic_meanshift(M, P , h, 0.0,0.0)
+    end
+    v=Tuple(P).-posmax
+    @test sum(abs.(v))<1e-10
 end
